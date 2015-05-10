0165c3c746ce7ce513465255f760f58a09e08eee
Michael Oryl <michael@oryl.com>
2015-05-09 22:40:00 -0400

    Manually setting collision detection props; analyzing the impact data

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ module.exports = function (my) {
     recent: 0
   };
 
+  var stopped = false;
   var moves = [];
   var movesQueue = [];
 
@@ -34,6 +35,12 @@ module.exports = function (my) {
       .then(resetColor());
   }
 
+  function flashBlue() {
+    my.sphero.setRGB(0x0000FF);
+    qt.after(500)
+      .then(resetColor());
+  }
+
   function calibrate(seconds) {
     my.sphero.startCalibration();
     after((seconds).seconds(), function () {
@@ -45,10 +52,14 @@ module.exports = function (my) {
     my.sphero.setRGB(0x402000);
   }
 
-  function collision() {
+  function collision(data) {
     console.info("Captain, we've been hit!");
     flashRed(1);
     my.sphero.stop();
+    stopped = true;
+    console.info(JSON.stringify(data));
+    var impact = convertCollisionData(data.DATA);
+    console.info(JSON.stringify(impact));
     collisionCount.total++;
     collisionCount.recent++;
     if (collisionCount.recent > 2) {
@@ -64,13 +75,28 @@ module.exports = function (my) {
         direction: direction,
         speed: lastMove.speed
       });
-      console.info("Preparing to alter course to " + direction + " degrees at speed of " + lastMove.speed);
+      //console.info("Preparing to alter course to " + direction + " degrees at speed of " + lastMove.speed);
+      qt.after((2).seconds()).then(function() {stopped = false;});
     }
+  }
+
+  function convertCollisionData(data) {
+    var obj = {};
+    obj.xPower = data[0] + data[1]*256;
+    obj.yPower = data[2] + data[3]*256;
+    obj.zPower = data[4] + data[5]*256;
+    obj.impactAxis = data[6];
+    obj.xImpact = data[7];
+    obj.yImpact = data[8];
+    obj.speed = data[9];
+
+    return obj;
   }
 
   function init() {
     console.info("Initializing application");
-    my.sphero.detectCollisions();
+    my.sphero.configureCollisionDetection(0x01, 0x40, 0x40, 0x40, 0x40, 0x50); // defaults: 0x01, 0x40, 0x40, 0x50, 0x50, 0x50
+
     //my.sphero.detectLocator();
     resetColor();
     /*
@@ -92,7 +118,7 @@ module.exports = function (my) {
     every((4).seconds(), function () {
       if (collisionCount.recent > 0) {
         collisionCount.recent--;
-        console.info('Decreasing collision count by one');
+        //console.info('Decreasing collision count by one');
       }
     });
 
@@ -100,14 +126,15 @@ module.exports = function (my) {
       var speed, direction;
       if (movesQueue.length == 0) { // if no moves have been queued up, pick a random course
         direction = Math.floor(Math.random() * 360);
-        speed = 50 + Math.floor(Math.random() * 75);
+        speed = 70 + Math.floor(Math.random() * 75);
       } else { // otherwise take the last course in the queue
         var nextMove = movesQueue.pop();
         direction = nextMove.direction;
         speed = nextMove.speed;
       }
       my.sphero.roll(speed, direction);
-      console.info("Setting new course to " + direction + " degrees at speed of " + speed);
+      flashBlue();
+      //console.info("Setting new course to " + direction + " degrees at speed of " + speed);
       moves.push({ // maintain log of previous moves for some future use
         direction: direction,
         speed: speed
